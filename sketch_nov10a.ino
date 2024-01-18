1ed1f7c1f602984cecc1532732234f8f56ffaa1b
SLX <126223816+slxyz2508@users.noreply.github.com>
2024-01-18 21:21:47 +0700

    Update sketch_nov10a.ino

--- sketch_nov10a.ino
+++ sketch_nov10a.ino
@@ -1,24 +1,50 @@
-#include <WiFi.h>
+#include <TridentTD_LineNotify.h>
 
-const char* ssid = "T1";
-const char* password = "06024917";
+#define SSID        "T1"                 
+#define PASSWORD    "06024917"          
+#define LINE_TOKEN  "BB8Y8aIkn0NsPym4k0UlQRN15ZcHtz40ODvhcZqSTLW"
 
-void setup(){
-    Serial.begin(115200);
-    delay(1000);
+int motionPin = 34;   
+int smokePin = 35;   
+void setup() {
+  Serial.begin(115200);
+  Serial.println();
+  Serial.println(LINE.getVersion());
 
-    WiFi.mode(WIFI_STA); //Optional
-    WiFi.begin(ssid, password);
-    Serial.println("\nConnecting");
+  WiFi.begin(SSID, PASSWORD);
+  Serial.printf("WiFi connecting to %s\n", SSID);
 
-    while(WiFi.status() != WL_CONNECTED){
-        Serial.print(".");
-        delay(100);
-    }
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(400);
+  }
 
-    Serial.println("\nConnected to the WiFi network");
-    Serial.print("Local ESP32 IP: ");
-    Serial.println(WiFi.localIP());
+  Serial.printf("\nWiFi connected\nIP : ");
+  Serial.println(WiFi.localIP());
+
+  
+  LINE.setToken(LINE_TOKEN);
+
+  LINE.notify("พร้อมใช้งาน");
+  pinMode(motionPin, INPUT);
+  pinMode(smokePin, INPUT);
+
+  Serial.println("ready input");
 }
 
-void loop(){}
+void loop() {
+  int motionValue = digitalRead(motionPin);
+
+  if (motionValue == HIGH) {
+    Serial.println("Motion detected!");
+    int smokeValue = analogRead(smokePin);
+
+    if (smokeValue == LOW) {
+      LINE.notify("มีคนสูบบุหรี่");
+      Serial.println("Smoking");
+    } else {
+      Serial.println("No Smoking");
+    }
+}
+  } else {
+    Serial.println("No motion");}}
